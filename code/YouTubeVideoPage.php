6c1e415dd04292171b05fa2f22307066601a5548
helpfulrobot <chris@silverstripe.com>
2016-01-01 06:29:05 +1300

    Converted to PSR-2

--- code/YouTubeVideoPage.php
+++ code/YouTubeVideoPage.php
@@ -1,42 +1,45 @@
 <?php
-class YouTubeVideoPage extends Page {
-	public static $db = array(
-	'Bottom' => 'Boolean',
-	'BackgroundColour' => 'VarChar(7)',
-	'TextColour' => 'VarChar(7)',
-	);
+class YouTubeVideoPage extends Page
+{
+    public static $db = array(
+    'Bottom' => 'Boolean',
+    'BackgroundColour' => 'VarChar(7)',
+    'TextColour' => 'VarChar(7)',
+    );
 
-	public static $has_one = array(
-	);
-	
-	public static $has_many = array(
-		"Video" => "Video"
-	);
+    public static $has_one = array(
+    );
+    
+    public static $has_many = array(
+        "Video" => "Video"
+    );
 
-	function getCMSFields() {
-		$fields = parent::getCMSFields();
-		
-		//People
-		$videoTable = new DataObjectManager(
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+        
+        //People
+        $videoTable = new DataObjectManager(
             $this,
             'Video',
             'Video',
             array(
                 'VideoName' => 'Name',
-				'URL' => 'URL'
+                'URL' => 'URL'
             ),
             'getCMSFields_forPopup'
         );
-        $videoTable->setAddTitle('Video'); 
-		
+        $videoTable->setAddTitle('Video');
+        
         $fields->addFieldToTab('Root.Content.Video', $videoTable);
-		$fields->addFieldToTab('Root.Content.Layout', new CheckboxField('Bottom', 'Display playlist at the bottom'));
-		$fields->addFieldToTab("Root.Content.Layout", new TextField("BackgroundColour", 'Hex colour code for Background Colour', '', 7));
-		$fields->addFieldToTab("Root.Content.Layout", new TextField("TextColour", 'Hex colour code for Text Colour', '', 7));
-		
-		return $fields;
-	}
+        $fields->addFieldToTab('Root.Content.Layout', new CheckboxField('Bottom', 'Display playlist at the bottom'));
+        $fields->addFieldToTab("Root.Content.Layout", new TextField("BackgroundColour", 'Hex colour code for Background Colour', '', 7));
+        $fields->addFieldToTab("Root.Content.Layout", new TextField("TextColour", 'Hex colour code for Text Colour', '', 7));
+        
+        return $fields;
+    }
 }
 
-class YouTubeVideoPage_Controller extends Page_Controller {
-} 
+class YouTubeVideoPage_Controller extends Page_Controller
+{
+}
